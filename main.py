e9461b436dd0be694caef1dbfc8f593a3e7398b5
Chad <101136336+WxProg@users.noreply.github.com>
2022-10-02 12:54:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import art
+from replit import clear
+
+print(art.logo)
+print("Hello General! The camp has been inflitrated.")
+print("Use this cipher to communicate with our allies.")
+
+restart_program = True
+
+while restart_program: 
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 
+     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    condition = True
+    list_of_decision = ["encrypt", "decrypt"]
+    e = "encryption"
+    while condition:
+        decision = input("\nWould you like to 'encrypt' or 'decrypt'?\n")   
+        if decision in list_of_decision:
+            text = input("\nType your message:\n").lower()
+            shift = int(input("\nType the shift number:\n"))
+            condition = False
+        else:
+            print("Wrong input. Try again!")
+            
+        def caesar(text, shift, decision):
+            clear()
+            final_text = " "
+            if decision == "decrypt":
+                shift *= -1
+            for char in text:
+                if char not in alphabet:
+                    final_text += char
+                else:
+                    current_postion = alphabet.index(char)
+                    new_position = current_postion + shift    
+                    final_text += alphabet[new_position]
+            print(f"The {decision}ed text is:{final_text}\n")
+            if decision == "encrypt":
+                if len(final_text) <= 6:
+                    print(f"Weak {decision}ion. Message maybe easily intercepted.\n")
+                else:
+                    print(f"Strong {decision}ion. Message will be hard to intercept.\n")
+            
+    caesar(text, shift, decision)
+    
+    if input("Would you like to restart cipher? 'Yes' or 'No' \n").lower() == "yes":
+        restart_program = True
+    else:
+        restart_program = False
+        print("Program terminated. Good Bye. ")
+ 
